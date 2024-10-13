1a72ba9e7277ee2b288aae234cfa2861be09f4fa
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-13 13:01:20 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,12 @@
+// const num1 = 12;
+// const num2 = 22;
+// const num3 = 44;
+// const num4 = 442;
+
+//const number = 12588551112; // not array
+
+// (),{},not array // []-.>> array bracket
+const numbers = [12, 22, 45, 65]; ---> array example
+const friends = ['abul', 'babklu', 'rudu'] ---> string type array
+const passed = [true, false,true, fales ]; ---> bollean type array
+const mixedArray = [12, 'pappu',true,45.566]---> mixed type array
